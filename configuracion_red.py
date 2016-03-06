50bb9d670316cc59cb19a6029ee85e3d7c950e1b
Guillermo García López <memowii@hotmail.com>
2016-03-06 11:17:35 -0600

    Change some typos and add code for dynamic configuration

--- configuracion_red.py
+++ configuracion_red.py
@@ -15,17 +15,24 @@ respuesta = raw_input()
 if respuesta == 'e':
 	print "\nHa elegido realizar la configuración estática, por favor ingrese"
 	print "todos los datos que se le pidan."
-	print "\nIngrese el nombre de la interfaz: "
+	print "\nIngrese el nombre de la interfaz:",
 	interfaz = raw_input()
-	print "\nIngrese la ip del dispositivo: "
+	print "\nIngrese la ip del dispositivo:",
 	ip = raw_input()
-	print "\nIngrese la máscara de red: "
+	print "\nIngrese la máscara de red:",
 	mascara = raw_input()
-	print "\nIngrese el broadcast: "
+	print "\nIngrese el broadcast:",
 	broadcast = raw_input()
 	setencia_cli = "ifconfig "+interfaz+" "+ip+" netmask "+mascara+" broadcast "+broadcast+" up"
 	system(setencia_cli)
 	print "\nAhora se creara la tabla de enrutamiento.\n"
 	system('route')
 	print "\nLa configuración estática ha sido terminada."
-
+else:
+	print "\nHa elegido realizar la configuración dinámica."
+	print "\nIntroduzca el nombre de la intefaz que quiere configurar:",
+	interfaz = raw_input()
+	print "\nSe llamará a un comando externo de Linux para que realize la"
+	print "configuración."
+	setencia_cli = "dhclient -v "+interfaz
+	print "La configuración dinámica ha sido terminada."
