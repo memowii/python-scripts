6d01f90cbea293bf6cfcfb4b52e4f4d222724cfb
Guillermo García López <memogl25@gmail.com>
2017-11-10 13:38:57 -0600

    Add the group parameter; reformat the code

--- make_website.py
+++ make_website.py
@@ -3,19 +3,27 @@ import socket
 
 from subprocess import run
 
-parser = argparse.ArgumentParser(description="set up an apache2 virtual host (Ubuntu 14 and 16).")
+parser = argparse.ArgumentParser(description="set up an apache2 virtual host for Ubuntu 14 and 16")
 parser.add_argument("host", help="URL of the host, e.g., test.dev")
-parser.add_argument("user", help="user name who will be the owner of the created folder")
+parser.add_argument("user", help="owner-user of the created project directory")
+parser.add_argument("group", nargs='?',
+                    help="(optional) owner-group of the created project directory, if absent owner-user will be used")
 parser.add_argument("-r", "--rewrite", action="store_true", help="enable URL rewrites")
 parser.add_argument("-e", "--example-index", action="store_true", help="create an index.php example")
 args = parser.parse_args()
 
 host = args.host
 user = args.user
+group = args.group
 
 run(['mkdir', '-p', '/var/www/' + host])
 
-run(['chown', '-R', user + ':' + user, '/var/www/' + host])
+create_directory_project_command = 'chown -R {0}:{0} /var/www/{1}'.format(user, host)
+
+if group:
+    create_directory_project_command = create_directory_project_command.replace(':' + user, ':' + group)
+
+run([create_directory_project_command], shell=True)
 
 if args.example_index:
     example_index = """
@@ -24,7 +32,7 @@ if args.example_index:
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<h1>It works!</h1>'; ?> 
+ <?php echo '<h1>It works!</h1>'; ?>
  </body>
 </html>
 """
@@ -50,7 +58,7 @@ if args.rewrite:
     AllowOverride All
     Require all granted
 </Directory>
-    
+
 {1}
 """.format(host, conf_content)
 
@@ -60,8 +68,8 @@ run(['a2ensite', host + '.conf'])
 
 run(['service apache2 restart'], shell=True)
 
-ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
+    [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
+     [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
-run(["sed -i '1i{0} {1}\n # Added by script .py' /etc/hosts".format(ip, host)], shell=True)
-
-# todo list: 1.tomar argumentos del usuario (host, user, rewrite?, index?) 2. poder remove lo que se creo
+run(["sed -i '1i{0} {1}\n # Added by script {2}' /etc/hosts".format(ip, host, parser.prog)], shell=True)
