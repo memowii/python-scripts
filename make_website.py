4de547226a13ac2c8b61e26264c4ca8444c5aa3d
Guillermo García López <memogl25@gmail.com>
2017-11-10 18:15:16 -0600

    Break the script into functions

--- make_website.py
+++ make_website.py
@@ -3,35 +3,44 @@ from argparse import ArgumentParser
 from subprocess import run
 from subprocess import DEVNULL
 from sys import exit
+from os.path import realpath
+from os.path import expanduser
 
-parser = ArgumentParser(description="set up an apache2 virtual host for Ubuntu 14 and 16")
-parser.add_argument("host", help="URL of the host, e.g., test.dev")
-parser.add_argument("user", help="owner-user of the created project directory")
-parser.add_argument("group", nargs='?',
-                    help="(optional) owner-group of the created project directory, if absent owner-user will be used")
-parser.add_argument("-r", "--rewrite", action="store_true", help="enable URL rewrites")
-parser.add_argument("-e", "--example-index", action="store_true", help="create an index.php example")
-args = parser.parse_args()
 
-host = args.host
-user = args.user
-group = args.group
+def handle_input():
+    parser = ArgumentParser(description="set up an apache2 virtual host for Ubuntu 14 and 16")
 
-completed_process = run(['mkdir', '/var/www/' + host], stderr=DEVNULL)
+    parser.add_argument("host", help="URL of the host, e.g., test.dev")
+    parser.add_argument("user", help="owner-user of the created project directory")
+    parser.add_argument("group", nargs='?',
+                        help="(optional) owner-group of the created project directory, if absent owner-user will be used")
+    parser.add_argument("-r", "--rewrite", action="store_true", help="enable URL rewrites")
+    parser.add_argument("-e", "--example-index", action="store_true", help="create an index.php example")
 
-if completed_process.returncode:
-    exit("{0} cannot create directory '/var/www/{1}' already exists: enter a different host name".format(parser.prog,
-                                                                                                         host))
+    return parser.parse_args()
 
-create_directory_project_command = 'chown -R {0}:{0} /var/www/{1}'.format(user, host)
 
-if group:
-    create_directory_project_command = create_directory_project_command.replace(':' + user, ':' + group)
+def create_virtual_host(args):
+    host = args.host
+    user = args.user
+    group = args.group
+    exec_script_path = realpath(expanduser(__file__))
 
-run([create_directory_project_command], shell=True)
+    completed_process = run(['mkdir', '/var/www/' + host], stderr=DEVNULL)
 
-if args.example_index:
-    example_index = """
+    if completed_process.returncode:
+        exit("{0} cannot create directory '/var/www/{1}' already exists: enter a different host name".format(
+            exec_script_path, host))
+
+    create_directory_project_command = 'chown -R {0}:{0} /var/www/{1}'.format(user, host)
+
+    if group:
+        create_directory_project_command = create_directory_project_command.replace(':' + user, ':' + group)
+
+    run([create_directory_project_command], shell=True)
+
+    if args.example_index:
+        example_index = """
 <html>
  <head>
   <title>PHP Test</title>
@@ -40,10 +49,10 @@ if args.example_index:
  <?php echo '<h1>It works!</h1>'; ?>
  </body>
 </html>
-"""
-    run(['echo "' + example_index + '" > /var/www/' + host + '/index.php'], shell=True)
+    """
+        run(['echo "' + example_index + '" > /var/www/' + host + '/index.php'], shell=True)
 
-conf_content = """
+    conf_content = """
 <VirtualHost *:80>
     ServerAdmin admin@{0}
     ServerName {0}
@@ -54,10 +63,10 @@ conf_content = """
 </VirtualHost>
 
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
-""".format(host)
+    """.format(host)
 
-if args.rewrite:
-    conf_content = """
+    if args.rewrite:
+        conf_content = """
 <Directory /var/www/{0}>
     Options Indexes FollowSymLinks
     AllowOverride All
@@ -65,16 +74,24 @@ if args.rewrite:
 </Directory>
 
 {1}
-""".format(host, conf_content)
+    """.format(host, conf_content)
 
-run(['echo "' + conf_content + '" > /etc/apache2/sites-available/' + host + '.conf'], shell=True)
+    run(['echo "' + conf_content + '" > /etc/apache2/sites-available/' + host + '.conf'], shell=True)
 
-run(['a2ensite', host + '.conf'])
+    run(['a2ensite', host + '.conf'])
 
-run(['service apache2 restart'], shell=True)
+    run(['service apache2 restart'], shell=True)
 
-ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
-    [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
-     [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+    ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
+        [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
+         [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
-run(["sed -i '1i{0} {1} # Added by script {2}\n' /etc/hosts".format(ip, host, parser.prog)], shell=True)
+    run(["sed -i '1i{0} {1} # Added by script {2}\n' /etc/hosts".format(ip, host, exec_script_path)], shell=True)
+
+
+def main():
+    args = handle_input()
+    create_virtual_host(args)
+
+
+main()
