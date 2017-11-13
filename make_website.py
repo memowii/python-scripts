484881980e290c580f952fc25df7b6d11be2fc3b
Guillermo García López <memogl25@gmail.com>
2017-11-13 12:11:59 -0600

    Add the option to remove a host; Add the function get_ip; change " with '

--- make_website.py
+++ make_website.py
@@ -10,14 +10,23 @@ from os.path import expanduser
 def handle_input():
     parser = ArgumentParser(description="set up an apache2 virtual host for Ubuntu 14 and 16")
 
-    parser.add_argument("host", help="URL of the host, e.g., test.dev")
-    parser.add_argument("user", help="owner-user of the created project directory")
-    parser.add_argument("group", nargs='?',
-                        help="(optional) owner-group of the created project directory, if absent owner-user will be used")
-    parser.add_argument("-r", "--rewrite", action="store_true", help="enable URL rewrites")
-    parser.add_argument("-e", "--example-index", action="store_true", help="create an index.php example")
+    parser.add_argument('host', help='URL of the host, e.g., test.dev', nargs='?')
+    parser.add_argument('user', help='owner-user of the created project directory', nargs='?')
+    parser.add_argument('group', nargs='?',
+                        help='(optional) owner-group of the created project directory, if absent owner-user will be used')
+    parser.add_argument('-r', '--rewrite', action='store_true', help='enable URL rewrites')
+    parser.add_argument('-e', '--example-index', action='store_true', help='create an index.php example')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-d', '--delete', help='delete a virtual host from /var/www/')
 
     return parser.parse_args()
+
+
+def get_ip():
+    return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
+        [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
+         [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
 
 def create_virtual_host(args):
@@ -78,20 +87,33 @@ def create_virtual_host(args):
 
     run(['echo "' + conf_content + '" > /etc/apache2/sites-available/' + host + '.conf'], shell=True)
 
-    run(['a2ensite', host + '.conf'])
+    run(['a2ensite', host + '.conf'], stdout=DEVNULL)
 
     run(['service apache2 restart'], shell=True)
 
-    ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
-        [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
-         [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+    ip = get_ip()
 
     run(["sed -i '1i{0} {1} # Added by script {2}\n' /etc/hosts".format(ip, host, exec_script_path)], shell=True)
+
+    print('host {} has been created'.format(host))
+
+
+def delete_host(host):
+    run(['rm', '-rf', '/var/www/{}'.format(host)])
+    run(['a2dissite', '{}.conf'.format(host)], stdout=DEVNULL)
+    run(['rm', '/etc/apache2/sites-available/{}.conf'.format(host)])
+    run(['sed', '-i', '/{}/d'.format(host), '/etc/hosts'])
+    run(['service', 'apache2', 'restart'])
+    print('host {} has been removed'.format(host))
 
 
 def main():
     args = handle_input()
-    create_virtual_host(args)
+
+    if args.delete:
+        delete_host(args.delete)
+    else:
+        create_virtual_host(args)
 
 
 main()
