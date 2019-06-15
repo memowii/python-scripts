493136a599cbd9f5d879be5f845f541f679ffbaa
Guillermo García López <memogl25@gmail.com>
2019-06-15 10:23:38 -0500

    make create_index function

--- platzi-content-structure.py
+++ platzi-content-structure.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 
+
 class PlatziClass:
     PYTHON_PARSER = 'html.parser'
     RESPONSE_OK = 200
@@ -85,32 +86,80 @@ class PlatziLesson:
 
 
 class Markdown:
-    
+    SPECIAL_CHARS_DICT = {
+        'á': '%C3%A1',
+        'é': '%C3%A9',
+        'í': '%C3%AD',
+        'ó': '%C3%B3',
+        'ú': '%C3%BA',
+        'ñ': '%C3%B1'
+    }
+
+    @staticmethod
     def h1(text, toc=False):
         return '# {}{}'.format(text, ' <!-- omit in toc -->' if toc else '')
 
+    @staticmethod
     def h2(text, toc=False):
         return '## {}{}'.format(text, ' <!-- omit in toc -->' if toc else '')
 
+    @staticmethod
     def h3(text, toc=False):
         return '### {}{}'.format(text, ' <!-- omit in toc -->' if toc else '')
 
+    @staticmethod
     def h4(text, toc=False):
         return '#### {}{}'.format(text, ' <!-- omit in toc -->' if toc else '')
 
+    @staticmethod
     def hr():
         return '---'
 
+    @staticmethod
     def link(text, href):
         return '[{}]({})'.format(text, href)
 
-# print(Markdown.h1('Curso de Programación en Bash Shell'))
-# print(Markdown.link("I'm an inline-style link", 'https://www.google.com'))
-# print(Markdown.h1(Markdown.link('Introducción a Terminal y Línea de Comandos', 'https://platzi.com/clases/bash-shell/')))
+    @staticmethod
+    def slug(text):
+        word_array = Markdown.get_word_array(text)
+        return '#' + '-'.join(word_array)
+
+    @staticmethod
+    def change_special_chars(text):
+        changed_text = text
+        for special_char, symbol in Markdown.SPECIAL_CHARS_DICT.items():
+            if special_char in changed_text:
+                changed_text = changed_text.replace(special_char, symbol)
+        return changed_text
+
+    @staticmethod
+    def get_word_array(text):
+        word_array = []
+        i = 0
+        while i < len(text):
+            if text[i].isalpha():
+                word = ''
+                while i < len(text) and text[i].isalpha():
+                    word += text[i]
+                    i += 1
+                changed_word = Markdown.change_special_chars(word.lower())
+                word_array.append(changed_word)
+            i += 1
+        return word_array
+
+    @staticmethod
+    def index_link(text):
+        return Markdown.link(text, Markdown.slug(text))
+
 
 class Readme:
     FILE_NAME = 'readme.md'
     FILE_MODE_W = 'w'
+    RESOURCES_LINE = 'Resources:'
+    NEW_LINE_CHAR = '\n'
+    INDEX_INDENTATION_LEVEL_1 = '* '
+    INDEX_INDENTATION_LEVEL_2 = '  - '
+    INDEX_INDENTATION_LEVEL_3 = '    + '
 
     def __init__(self, course_url):
         self.course_url = course_url
@@ -118,28 +167,55 @@ class Readme:
 
     def create_readme(self):
         file = open(Readme.FILE_NAME, Readme.FILE_MODE_W)
-        
-        link_class_title = Markdown.link(self.platzi_class.get_title(), self.platzi_class.url)
+
+        link_class_title = Markdown.link(self.platzi_class.get_title(),
+                                         self.platzi_class.url)
         class_title = Markdown.h1(link_class_title)
         file.write(class_title)
-        file.write('\n\n')
+        file.write(Readme.NEW_LINE_CHAR * 2)
+
+        file.write(self.create_index())
 
         modules = self.platzi_class.get_modules()
         for module in modules:
             module_title = Markdown.h2(module.get_title())
             file.write(module_title)
-            file.write('\n\n')
+            file.write(Readme.NEW_LINE_CHAR * 2)
 
             lessons = module.get_lessons()
             for lesson in lessons:
                 link_lesson_title = Markdown.link(lesson.get_title(), lesson.get_link())
                 lesson_title = Markdown.h3(link_lesson_title)
-                file.write(link_lesson_title)
-                file.write('\n\n\n\n')
+                file.write(lesson_title)
+                file.write(Readme.NEW_LINE_CHAR * 4)
                 file.write(Markdown.hr())
-                file.write('\n\n')
-        file.write(Markdown.h4('Resources:', True))
+                file.write(Readme.NEW_LINE_CHAR * 2)
+        file.write(Markdown.h4(Readme.RESOURCES_LINE, True))
         file.close()
+
+    def create_index(self):
+        index = ''
+
+        class_title = self.platzi_class.get_title()
+        index_class_title = Readme.INDEX_INDENTATION_LEVEL_1 + Markdown.index_link(class_title)
+        index += index_class_title
+        index += Readme.NEW_LINE_CHAR
+
+        modules = self.platzi_class.get_modules()
+        for module in modules:
+            module_title = module.get_title()
+            index_module_title = Readme.INDEX_INDENTATION_LEVEL_2 + Markdown.index_link(module_title)
+            index += index_module_title
+            index += Readme.NEW_LINE_CHAR
+
+            lessons = module.get_lessons()
+            for lesson in lessons:
+                lesson_title = lesson.get_title()
+                index_lesson_title = Readme.INDEX_INDENTATION_LEVEL_3 + Markdown.index_link(lesson_title)
+                index += index_lesson_title
+                index += Readme.NEW_LINE_CHAR
+        index += Readme.NEW_LINE_CHAR
+        return index
         
 
 readme = Readme('https://platzi.com/clases/bash-shell/')
